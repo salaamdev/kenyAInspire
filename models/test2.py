cf88811f2f096d45cc5da4f259710191e9ae9809
salaamdev <abdisalamxhassan@gmail.com>
2024-10-03 19:14:36 +0300

    removed unnecassary files + more dependencies

--- models/test2.py
+++
@@ -1,5 +0,0 @@
-# Load model directly
-from transformers import AutoModelForMaskedLM, AutoTokenizer
-
-tokenizer = AutoTokenizer.from_pretrained("distilbert/distilbert-base-uncased")
-model = AutoModelForMaskedLM.from_pretrained("distilbert/distilbert-base-uncased")
