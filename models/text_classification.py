f458e0e06c5499031a226e333b9f53b435ff7f06
salaamdev <abdisalamxhassan@gmail.com>
2024-10-06 22:49:44 +0300

    Add text_classification.py for fine-tuned text classification model

---
+++ models/text_classification.py
@@ -0,0 +1,94 @@
+from huggingface_hub import notebook_login
+
+
+notebook_login()
+
+
+from datasets import load_dataset
+
+
+imdb = load_dataset("imdb")
+
+
+imdb["test"][0]
+
+
+from transformers import AutoTokenizer
+
+
+tokenizer = AutoTokenizer.from_pretrained("distilbert/distilbert-base-uncased")
+
+
+def preprocess_function(examples):
+    return tokenizer(examples["text"], truncation=True)
+
+
+tokenized_imdb = imdb.map(preprocess_function, batched=True)
+
+
+from transformers import DataCollatorWithPadding
+
+data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
+
+
+import evaluate
+
+accuracy = evaluate.load("accuracy")
+
+
+import numpy as np
+
+
+def compute_metrics(eval_pred):
+    predictions, labels = eval_pred
+    predictions = np.argmax(predictions, axis=1)
+    return accuracy.compute(predictions=predictions, references=labels)
+
+
+id2label = {0: "NEGATIVE", 1: "POSITIVE"}
+label2id = {"NEGATIVE": 0, "POSITIVE": 1}
+
+
+from transformers import AutoModelForSequenceClassification, Trainer, TrainingArguments
+
+model = AutoModelForSequenceClassification.from_pretrained(
+    "distilbert/distilbert-base-uncased",
+    num_labels=2,
+    id2label=id2label,
+    label2id=label2id,
+)
+
+
+training_args = TrainingArguments(
+    output_dir="my_awesome_model",
+    learning_rate=2e-5,
+    per_device_train_batch_size=16,
+    per_device_eval_batch_size=16,
+    num_train_epochs=2,
+    weight_decay=0.01,
+    eval_strategy="epoch",
+    save_strategy="epoch",
+    load_best_model_at_end=True,
+    push_to_hub=True,
+)
+
+trainer = Trainer(
+    model=model,
+    args=training_args,
+    train_dataset=tokenized_imdb["train"],
+    eval_dataset=tokenized_imdb["test"],
+    tokenizer=tokenizer,
+    data_collator=data_collator,
+    compute_metrics=compute_metrics,
+)
+
+trainer.train()
+
+
+text = "horror movies are the best"
+
+
+from transformers import pipeline
+
+classifier = pipeline("sentiment-analysis", model="./my_awesome_model")
+classifier(text)
